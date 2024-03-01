51675886013efe9774d1115b8813465ee0fd0432
irrisdev <x3irris@proton.me>
2024-03-01 03:41:03 +0000

    Remove word after space

--- typing.js
+++ typing.js
@@ -7,15 +7,27 @@ document.addEventListener("DOMContentLoaded", async () => {
     const words = await test();
 
     const root = document.getElementById("words-container");
-    let newSpan;
+    let newSpan, counter = 0;
 
     for(let word of words){
-
         newSpan = document.createElement("span");
+        newSpan.id = 'w' + counter;
         newSpan.textContent = word;
         root.append(newSpan);
+        counter++;
+
 
     }
+    let removeCounter = 0;
+    const removeSpan = () => {
+        document.getElementById("w" + removeCounter).remove();
+    }
+    const input = document.getElementById("input");
+    input.addEventListener("keydown", (e) => {
+        if(`${e.code}` == "Space"){
+            input.value = "";
+        };
+    })
 
 });
 
